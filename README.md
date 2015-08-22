67537b24cc44c36d32384e2e88e23e86ecad21a5
Jax <jax.gibb@gmail.com>
2015-08-22 18:48:08 +1000

    Extra comments.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Submitted by Jax for Hometrack.
 Frighteningly, I used to set a similar code exercise for all potential employees, on the basis that if they couldn't follow instructions & get some code over the line without me staring at them, then there was probably something wrong that I couldn't fix with training.
 
 I made a few assumptions & executive decisions with the implementation:
+* if one of the entries is missing a required field, then the whole request is invalid
 * if an address field is missing, then it is simply not included in the result - as opposed, the address is invalid.
 * only the "interesting" fields are concatenated - I dropped off lat & long.
 
--- hometrack.js
+++ hometrack.js
@@ -8,42 +8,47 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function (req, res) {
 	var valid = false;
 	var completed = [];
-  if (req.body.hasOwnProperty("payload")) {
-	  valid = true;
-	var payload = req.body["payload"];
-	for(var i = 0; i < payload.length; i++) {
-		if (payload[i].hasOwnProperty("workflow") && payload[i].hasOwnProperty("type")) {
-			if ((payload[i]["workflow"] == "completed") && (payload[i]["type"] == "htv")) {
-				var concatAddress = "";
-				if (payload[i]["address"].hasOwnProperty("buildingNumber")) {
-					concatAddress += payload[i]["address"]["buildingNumber"];
+	if (req.body.hasOwnProperty("payload")) {
+		valid = true;
+		var payload = req.body["payload"];
+		for(var i = 0; i < payload.length; i++) {
+			// look for a match on the required fields
+			if (payload[i].hasOwnProperty("workflow") && payload[i].hasOwnProperty("type")) {
+				if ((payload[i]["workflow"] == "completed") && (payload[i]["type"] == "htv")) {
+					// build up the new address field from anything we find
+					var concatAddress = "";
+					if (payload[i]["address"].hasOwnProperty("buildingNumber")) {
+						concatAddress += payload[i]["address"]["buildingNumber"];
+					}
+					if (payload[i]["address"].hasOwnProperty("street")) {
+						concatAddress += " " + payload[i]["address"]["street"];
+					}
+					if (payload[i]["address"].hasOwnProperty("suburb")) {
+						concatAddress += " " + payload[i]["address"]["suburb"];
+					}
+					if (payload[i]["address"].hasOwnProperty("state")) {
+						concatAddress += " " + payload[i]["address"]["state"];
+					}
+					if (payload[i]["address"].hasOwnProperty("postcode")) {
+						concatAddress += " " + payload[i]["address"]["postcode"];
+					}
+					
+					// build the new record, adding to the result array
+					var match = { concataddress: concatAddress, type: payload[i]["type"], workflow: payload[i]["workflow"] };
+					completed.push(match);
 				}
-				if (payload[i]["address"].hasOwnProperty("street")) {
-					concatAddress += " " + payload[i]["address"]["street"];
-				}
-				if (payload[i]["address"].hasOwnProperty("suburb")) {
-					concatAddress += " " + payload[i]["address"]["suburb"];
-				}
-				if (payload[i]["address"].hasOwnProperty("state")) {
-					concatAddress += " " + payload[i]["address"]["state"];
-				}
-				if (payload[i]["address"].hasOwnProperty("postcode")) {
-					concatAddress += " " + payload[i]["address"]["postcode"];
-				}
-				var match = { concataddress: concatAddress, type: payload[i]["type"], workflow: payload[i]["workflow"] };
-				completed.push(match);
-			}
-		} else {
-			valid = false;
-		}	
+			} else {
+				valid = false;
+			}	
+		}
 	}
-  }
-  if (valid) {
+	// respond with good addresses, or else an error
+	if (valid) {
 		res.json({ response: completed });
-  } else {
+	} else {
 		res.status(400);
 		res.json({ "error": "Could not decode request: JSON parsing failed" });
-  }
+	}
 });
 
 app.listen(process.env.PORT || 9999);
